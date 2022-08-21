c28f675b33d63a5c282b2b9621b0f422710ccf21
Thanit Konmek <thanit@frappet.com>
2022-08-21 12:44:17 +0700

    เพิ่ม google tag

--- pages/_document.js
+++ pages/_document.js
@@ -12,11 +12,20 @@ export default class MyDocument extends Document {
         return (
             <Html lang="en">
                 <Head>
+                    <script dangerouslySetInnerHTML={{
+                        __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer',GTM-TM7H4FZ');`}}></script>
                     <meta name="google-site-verification" content="YsuK3sIrMfpQY931poATkUIjgFaaxlPqJUxjUjGBP6k" />
                     <meta name="theme-color" content={theme.palette.primary.background} />
                     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Kanit:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" />
                 </Head>
                 <body>
+                    <noscript dangerouslySetInnerHTML={{
+                        __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TM7H4FZ"
+height="0" width="0" style="display:none;visibility:hidden"></iframe>`}}></noscript>
                     <Main />
                     <NextScript />
                 </body>
