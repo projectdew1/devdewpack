b77d048315a7de71f69dd885478b750f4266738c
Thanit Konmek <thanit@frappet.com>
2023-03-02 00:53:08 +0700

    เปลี่ยนการส่ง data add edit product

--- pages/_app.js
+++ pages/_app.js
@@ -11,56 +11,56 @@ import "../node_modules/@fortawesome/fontawesome-free/js/solid"
 import "../node_modules/@fortawesome/fontawesome-free/js/fontawesome"
 import "../node_modules/slick-carousel/slick/slick.css"
 import "../node_modules/slick-carousel/slick/slick-theme.css"
-import "../node_modules/react-image-gallery/styles/css/image-gallery.css";
+import "../node_modules/react-image-gallery/styles/css/image-gallery.css"
 import "../styles/globals.css"
 import "antd/dist/antd.css"
-import '@react-page/editor/lib/index.css';
-import '@react-page/plugins-background/lib/index.css';
-import '@react-page/plugins-html5-video/lib/index.css'
-import '@react-page/plugins-image/lib/index.css'
-import '@react-page/plugins-spacer/lib/index.css'
-import '@react-page/plugins-video/lib/index.css';
-import AOS from 'aos';
-import 'aos/dist/aos.css';
-import TagManager from 'react-gtm-module';
+import "@react-page/editor/lib/index.css"
+import "@react-page/plugins-background/lib/index.css"
+import "@react-page/plugins-html5-video/lib/index.css"
+import "@react-page/plugins-image/lib/index.css"
+import "@react-page/plugins-spacer/lib/index.css"
+import "@react-page/plugins-video/lib/index.css"
+import AOS from "aos"
+import "aos/dist/aos.css"
+import TagManager from "react-gtm-module"
 
 import { wrapper } from "../redux"
 
 export const cache = createCache({ key: "css", prepend: true })
 
 function MyApp(props) {
-    const { Component, pageProps } = props
+	const { Component, pageProps } = props
 
-    React.useEffect(() => {
-        // Remove the server-side injected CSS.
-        const jssStyles = document.querySelector("#jss-server-side")
-        AOS.init();
-        if (jssStyles) {
-            jssStyles.parentElement.removeChild(jssStyles)
-        }
-        TagManager.initialize({ gtmId: 'GTM-TM7H4FZ' });
-    }, [])
+	React.useEffect(() => {
+		// Remove the server-side injected CSS.
+		const jssStyles = document.querySelector("#jss-server-side")
+		AOS.init()
+		if (jssStyles) {
+			jssStyles.parentElement.removeChild(jssStyles)
+		}
+		TagManager.initialize({ gtmId: "GTM-TM7H4FZ" })
+	}, [])
 
-    return (
-        <CacheProvider value={cache}>
-            <Head>
-                <meta charSet="utf-8" />
-                <meta name="google-site-verification" content="YsuK3sIrMfpQY931poATkUIjgFaaxlPqJUxjUjGBP6k" />
-                <meta name="viewport" content="initial-scale=1, width=device-width" />
-                <link rel="icon" href="/kmsicon.ico" />
-            </Head>
-            <ThemeProvider theme={theme}>
-                {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-                <CssBaseline />
-                <Component {...pageProps} />
-            </ThemeProvider>
-        </CacheProvider>
-    )
+	return (
+		<CacheProvider value={cache}>
+			<Head>
+				<meta charSet="utf-8" />
+				<meta name="google-site-verification" content="YsuK3sIrMfpQY931poATkUIjgFaaxlPqJUxjUjGBP6k" />
+				<meta name="viewport" content="initial-scale=1, width=device-width" />
+				<link rel="icon" href="/kmsicon.ico" />
+			</Head>
+			<ThemeProvider theme={theme}>
+				{/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+				<CssBaseline />
+				<Component {...pageProps} />
+			</ThemeProvider>
+		</CacheProvider>
+	)
 }
 
 MyApp.propTypes = {
-    Component: PropTypes.elementType.isRequired,
-    pageProps: PropTypes.object.isRequired,
+	Component: PropTypes.elementType.isRequired,
+	pageProps: PropTypes.object.isRequired,
 }
 
 export default wrapper.withRedux(MyApp)
--- setApi/Config.js
+++ setApi/Config.js
@@ -1,10 +1,10 @@
 const master = "authorization"
 
-// const hosting = "https://localhost:5001"
-// const ImageHosting = "https://localhost:5001"
+const hosting = "https://localhost:5001"
+const ImageHosting = "https://localhost:5001"
 
-const hosting = "https://kmspacking.com:5003"
-const ImageHosting = "https://kmspacking.com:5003"
+// const hosting = "https://kmspacking.com:5003"
+// const ImageHosting = "https://kmspacking.com:5003"
 const host = hosting + "/api"
 const admin = host + "/admin"
 const contact = host + "/contact"
@@ -13,68 +13,68 @@ const page = host + "/page"
 const blog = host + "/blog"
 
 const api = {
-    mock: category + "/get",
-    base64: category + "/get64base",
-    admin,
-    login: admin + "/login",
-    user: admin + "/user",
-    adduser: admin + "/addUser",
-    deleteuser: admin + "/deleteUser",
-    reset: admin + "/reset",
-    change: admin + "/change",
-    ///Contact
-    contact,
-    addcontact: contact + "/addContact",
-    updatecontact: contact + "/updateRead",
-    deletecontact: contact + "/deleteContact",
-    // category
-    category,
-    addcategory: category + "/addCategory",
-    deletecategory: category + "/deleteCategory",
-    idcategory: category + "/idCategory",
-    updatecategory: category + "/update",
-    //type
-    type: category + "/type",
-    addtype: category + "/addType",
-    deletetype: category + "/deleteType",
-    idtype: category + "/idType",
-    updatetype: category + "/updateType",
+	mock: category + "/get",
+	base64: category + "/get64base",
+	admin,
+	login: admin + "/login",
+	user: admin + "/user",
+	adduser: admin + "/addUser",
+	deleteuser: admin + "/deleteUser",
+	reset: admin + "/reset",
+	change: admin + "/change",
+	///Contact
+	contact,
+	addcontact: contact + "/addContact",
+	updatecontact: contact + "/updateRead",
+	deletecontact: contact + "/deleteContact",
+	// category
+	category,
+	addcategory: category + "/addCategory",
+	deletecategory: category + "/deleteCategory",
+	idcategory: category + "/idCategory",
+	updatecategory: category + "/update",
+	//type
+	type: category + "/type",
+	addtype: category + "/addType",
+	deletetype: category + "/deleteType",
+	idtype: category + "/idType",
+	updatetype: category + "/updateType",
 
-    //Product
-    machine: category + "/machine",
-    addmachine: category + "/addMachine",
-    deletemachine: category + "/deleteMachine",
-    idmachine: category + "/idMachine",
-    updatemachine: category + "/updateMachine",
+	//Product
+	machine: category + "/machine",
+	addmachine: category + "/addMachine",
+	deletemachine: category + "/deleteMachine",
+	idmachine: category + "/idMachine",
+	updatemachine: category + "/updateMachine",
 
-    //Technical
-    technical: category + "/technical",
-    addtechnical: category + "/addTechnical",
-    deletetechnical: category + "/deleteTechnical",
-    updatetechnical: category + "/updateTechnical",
+	//Technical
+	technical: category + "/technical",
+	addtechnical: category + "/addTechnical",
+	deletetechnical: category + "/deleteTechnical",
+	updatetechnical: category + "/updateTechnical",
 
-    // page
-    pageHeader: page + "/header",
-    pageIdHeader: page + "/idHeader",
-    pageProduct: page + "/product",
-    pageIdProduct: page + "/idproduct",
+	// page
+	pageHeader: page + "/header",
+	pageIdHeader: page + "/idHeader",
+	pageProduct: page + "/product",
+	pageIdProduct: page + "/idproduct",
 
-    //photo
-    photo: blog + "/photoall",
-    addPhoto: blog + "/addPhoto",
-    deletePhoto: blog + "/deletePhoto",
+	//photo
+	photo: blog + "/photoall",
+	addPhoto: blog + "/addPhoto",
+	deletePhoto: blog + "/deletePhoto",
 
-    //blog
-    contentBlog: blog + "/contentBlog",
-    addBlog: blog + "/addBlog",
-    updateBlog: blog + "/updateBlog",
-    deleteBlog: blog + "/deleteBlog",
-    idBlog: blog + "/idBlog",
+	//blog
+	contentBlog: blog + "/contentBlog",
+	addBlog: blog + "/addBlog",
+	updateBlog: blog + "/updateBlog",
+	deleteBlog: blog + "/deleteBlog",
+	idBlog: blog + "/idBlog",
 }
 
 export default {
-    master,
-    api,
-    hosting,
-    ImageHosting,
+	master,
+	api,
+	hosting,
+	ImageHosting,
 }
